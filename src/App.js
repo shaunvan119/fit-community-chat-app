6a1e6fdacd0d837452719f30041142933810bc7c
shaunvan119 <shaunvan@live.com.au>
2021-06-24 12:37:53 +1000

    added App css

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
 
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="cda420d1-2334-4a46-a2de-41631fe596a8"
+            userName="Fit-Chat-App"
+            userSecret="Goodlife1982"
+            />
+        
+    );
+}
+
+export default App;
