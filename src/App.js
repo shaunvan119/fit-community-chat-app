1b9d64b57c81d49ad8e5a07c514bb88d4b8d85cb
shaunvan119 <shaunvan@live.com.au>
2021-07-11 11:17:19 +1000

    Added logoin page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 
 import { ChatEngine } from 'react-chat-engine';
 
-import ChatFeed from './components/ChatFeed.jsx';
-
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
 
 const App = () => {
-    return (
-        <ChatEngine
-            height="100vh"
-            projectID="cda420d1-2334-4a46-a2de-41631fe596a8"
-            userName="Fit-Chat-App"
-            userSecret="Goodlife1982"
-            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps} />}
-            />
-        
-    );
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,10 +1,21 @@
-const MyMessage = () => {
+const MyMessage = ({message}) => {
+    if(message?.attachments?.lenght > 0 ) {
     return (
-        <div>
-            MyMessage
-        </div>
+            <img
+                src={message.attachments[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{ float: 'right'}}
+            />
+    )
+}
 
-    );
+return (
+    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+        {message.text}
+    </div>
+);
+
 }
 
 export default MyMessage;
